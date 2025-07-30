f29aa6b1aba960d1b17572fd14714e2df860324c
XcelevatorMaheshwari <maheshwari.2517@xcelevate.org>
2025-07-30 10:26:02 +0530

    print birth date in order

---
+++ FCP001.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class FCP001{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+sc.nextLine();
+String b=sc.nextLine();
+int c=sc.nextInt();
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+}}
