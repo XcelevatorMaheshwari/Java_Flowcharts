1f563657345ba49b6cbaec061015975d28374a62
XcelevatorMaheshwari <maheshwari.2517@xcelevate.org>
2025-07-31 10:03:22 +0530

    max of four numbers

---
+++ FCP015.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class FCP015{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+int d=sc.nextInt();
+int x=0;
+int z=0;
+if(a>b){
+ x=a;}
+else{
+x=b;
+}
+if(x>c){
+z=x;}
+else{
+z=c;
+}
+if(z>d){
+
+System.out.println(z);
+}
+    else{
+System.out.println(d);}
+}}
+
+
