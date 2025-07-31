260183facc12274ac9d62f9376274188e3d3a83d
XcelevatorMaheshwari <maheshwari.2517@xcelevate.org>
2025-07-31 09:56:43 +0530

    Largest divisible less than A

---
+++ FCP009.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class FCPOO9{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int x=a;
+int y=a%b;
+int z=x-y;
+System.out.println(z);
+}}
