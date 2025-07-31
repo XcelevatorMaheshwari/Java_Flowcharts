91f5324ed1f783f14b90d3d330a3c83e07b7bbf2
XcelevatorMaheshwari <maheshwari.2517@xcelevate.org>
2025-07-31 09:48:13 +0530

    Arithmetic operators

---
+++ FCP004.java
@@ -0,0 +1,11 @@
+public class FCP004{
+public static void main (String[]args){
+int a=12;
+int b=5;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a%b);
+System.out.println(a/b);
+}}
+
