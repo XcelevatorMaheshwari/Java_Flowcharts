4a6db56dcdde4f68cf5f63f227779e9a55d98d18
XcelevatorMaheshwari <maheshwari.2517@xcelevate.org>
2025-07-31 10:04:27 +0530

    leap year check

---
+++ FCP016.java
@@ -0,0 +1,10 @@
+public class FCP016{
+public static void main(String[]args){
+int x=2004;
+if((x  % 4 == 0 && x%100 != 0 )||(x % 400 == 0))
+{
+System.out.println("yes");
+}
+else{
+System.out.println("no");}
+}}
