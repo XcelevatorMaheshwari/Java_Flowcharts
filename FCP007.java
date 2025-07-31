cc3f231f4750beb41c02dc248faa4498be16a64b
XcelevatorMaheshwari <maheshwari.2517@xcelevate.org>
2025-07-31 09:54:18 +0530

    celsius to fahrenheit conversion

---
+++ FCP007.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class FCP007{
+public static void main(String[]args){
+Scanner scan=new Scanner(System.in);
+float c=scan.nextFloat();
+float f= (float)(((c*9)/5)+32);
+System.out.println(f);
+}
+}
